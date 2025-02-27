2f007be7234cf87ba5a38a8d3e4e563cc03c94f5
Abimbola Jolayemi <abimbolajolayemi111@yahoo.com>
2025-02-27 14:45:33 +0100

    fixed router in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import routes from '../routes/routes'
-
+import {useMemo} from 'react'
 const router = createBrowserRouter([...routes])
 
 function App() {
-
-  return (
-    <>
+    const appRouter = useMemo(()=>{
+      <>
     <RouterProvider router={router}/>
     </>
+    },[])
+      return (
+    {appRouter}
   )
 }
 
